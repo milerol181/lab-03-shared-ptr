73a2da68226b81c835a1810c7e03d05077070c14
milerol181 <milerol181@gmail.com>
2020-12-05 16:51:42 +0300

    first_commit

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,27 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+template <typename T>
+class SharedPtr {
+ public:
+  SharedPtr();
+  SharedPtr(T* ptr);
+  SharedPtr(const SharedPtr& r);
+  SharedPtr(SharedPtr&& r);
+  ~SharedPtr();
+  auto operator=(const SharedPtr& r) -> SharedPtr&;
+  auto operator=(SharedPtr&& r) -> SharedPtr&;
 
+  // проверяет, указывает ли указатель на объект
+  operator bool() const;
+  auto operator*() const -> T&;
+  auto operator->() const -> T*;
+
+  auto get() -> T*;
+  void reset();
+  void reset(T* ptr);
+  void swap(SharedPtr& r);
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+  auto use_count() const -> size_t;
+};
 #endif // INCLUDE_HEADER_HPP_
