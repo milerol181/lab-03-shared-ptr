e827f8597572d287c12d217e6dff6a0096a2eb09
milerol181 <milerol181@gmail.com>
2020-12-05 20:01:57 +0300

    second_commit

--- include/header.hpp
+++ include/header.hpp
@@ -2,27 +2,149 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <atomic>
+#include <iostream>
+#include <utility>
+
 template <typename T>
 class SharedPtr {
  public:
-  SharedPtr();
-  SharedPtr(T* ptr);
-  SharedPtr(const SharedPtr& r);
-  SharedPtr(SharedPtr&& r);
-  ~SharedPtr();
-  auto operator=(const SharedPtr& r) -> SharedPtr&;
-  auto operator=(SharedPtr&& r) -> SharedPtr&;
+  SharedPtr()
+  {
+    ptr = nullptr;
+    count_ptr = nullptr;
+  }
+  explicit SharedPtr(T* point)
+  {
+    ptr = point;
+    if (point == nullptr)
+    {
+      count_ptr = nullptr;
+    } else {
+      count_ptr = new std::atomic_uint;
+      *count_ptr = 1;
+    }
+  }
+  SharedPtr(const SharedPtr& r)
+  {
+    ptr = r.ptr;
+    count_ptr = r.count_ptr;
+    if (ptr) {
+      *count_ptr = *count_ptr + 1;
+    } else {
+      count_ptr = nullptr;
+    }
+  }
+  SharedPtr(SharedPtr&& r)
+  {
+    ptr = r.ptr;
+    count_ptr = r.count_ptr;
+    r.ptr = nullptr;
+    r.count_ptr = nullptr;
+  }
+  ~SharedPtr()
+  {
+    if (count_ptr){
+    if (*count_ptr==1)
+    {
+    delete ptr;
+    delete count_ptr;
+    } else {
+      *count_ptr = *count_ptr - 1;
+      }}
+      ptr = nullptr;
+      count_ptr = nullptr;
+  }
+  auto operator=(const SharedPtr& r) -> SharedPtr&
+  {
+    if (this != &r) {
+    ptr = r.ptr;
+    count_ptr = r.count_ptr;
+    if (ptr) {
+      *count_ptr = *count_ptr + 1;
+    } else {
+      count_ptr = nullptr;
+    }}
+    return *this;
+  }
+  auto operator=(SharedPtr&& r) -> SharedPtr&
+  {
+    if (this != &r){
+    ptr = r.ptr;
+    count_ptr = r.count_ptr;}
+    return *this;
+  }
 
   // проверяет, указывает ли указатель на объект
-  operator bool() const;
-  auto operator*() const -> T&;
-  auto operator->() const -> T*;
+  operator bool() const
+  {
+    if (ptr) return true;
+    else
+      return false;
+  }
+  {
+    if (ptr) return *ptr;
+    auto operator*() const -> T&
+    else
+      return nullptr;
+  }
+  auto operator->() const -> T*
+  {
+    return ptr;
+  }
 
-  auto get() -> T*;
-  void reset();
-  void reset(T* ptr);
-  void swap(SharedPtr& r);
+  auto get() -> T*
+  {
+    return ptr;
+  }
+  void reset()
+  {
+    if (*count_ptr==1)
+    {
+      delete ptr;
+      delete count_ptr;
+    } else {
+      *count_ptr = *count_ptr - 1;}
+    ptr = nullptr;
+    count_ptr = nullptr;
+  }
+  void reset(T* point)
+  {
+    if (*count_ptr==1)
+    {
+      delete ptr;
+      delete count_ptr;
+    } else {
+      *count_ptr = *count_ptr - 1;}
+    ptr = point;
+    if (point == nullptr)
+    {
+      count_ptr = nullptr;
+    } else {
+      count_ptr = new std::atomic_uint;
+      *count_ptr = 1;
+    }
+  }
+  void swap(SharedPtr& r)
+  {
+    T* tmp_ptr(std::move(r.ptr));
+    r.ptr = std::move(ptr);
+    ptr = std::move(tmp_ptr);
+    std::atomic_uint* tmp_count_ptr = r.count_ptr;
+    r.count_ptr = count_ptr;
+    count_ptr = tmp_count_ptr;
+  }
   // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
-  auto use_count() const -> size_t;
+  auto use_count() const -> size_t
+  {
+    if (count_ptr) {
+      return *count_ptr;
+    } else {return 0;}
+  }
+
+ private:
+    T* ptr;
+    std::atomic_uint* count_ptr;
+
 };
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -6,3 +6,69 @@
 TEST(Example, EmptyTest) {
     EXPECT_TRUE(true);
 }
+
+TEST(Example, first_test) {
+SharedPtr <int> ptr;
+int* tmp=ptr.get();
+EXPECT_EQ(tmp, nullptr);
+}
+
+TEST(Example, second_test) {
+  int *points = new int (24);
+
+  SharedPtr <int> ptr(points);
+  int* tmp=ptr.get();
+  EXPECT_EQ(tmp, points);
+}
+
+TEST(Example, third_test) {
+  int *points = nullptr;
+
+  SharedPtr <int> ptr(points);
+  int* tmp=ptr.get();
+  EXPECT_EQ(tmp, nullptr);
+}
+
+TEST(Example, fourth_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2 = ptr;
+  EXPECT_EQ(ptr.get(), points);
+}
+
+TEST(Example, fifth_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2 = ptr;
+  bool is_pointer = ptr2;
+  EXPECT_EQ(is_pointer, true);
+}
+
+class Test_class
+{
+ public:
+  int x;
+};
+
+TEST(Example, sixth_test) {
+  auto test_class_ptr = new Test_class;
+  test_class_ptr ->x=227;
+  SharedPtr <Test_class> ptr(test_class_ptr);
+  EXPECT_EQ(ptr->x, test_class_ptr->x);
+}
+
+TEST(Example, seventh_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2;
+  SharedPtr <int> ptr3=ptr2=ptr;
+  EXPECT_EQ(ptr3.get(), ptr.get());
+}
+
+TEST(Example, eight_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2=ptr;
+  SharedPtr <int> ptr3(std::move(ptr));
+  EXPECT_EQ(ptr3.use_count(), 2);
+}
